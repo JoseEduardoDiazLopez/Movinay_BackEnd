fb7327087886051381edd585bdebd80452c82518
SergioAlejandroGuzmanAlvarez <sealguzmanal@ittepci.edu.mx>
2023-10-17 08:02:18 -0700

    remove hour denuncias

--- models/Denuncias.js
+++ models/Denuncias.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const DenunciaSchema = new mongoose.Schema({
     idDenuncia: {type: Number},
     Fecha: {type: Date, default: Date.now()},
-    Hora: {type: Date.getTime()},
     Descripcion : {type: String},
     TipoDenuncia: {type: String}
   });
