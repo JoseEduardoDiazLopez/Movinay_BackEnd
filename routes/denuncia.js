4199a8aee39d589cf1ade7bbfc90a7a959b10931
SergioAlejandroGuzmanAlvarez <sealguzmanal@ittepci.edu.mx>
2023-10-17 07:51:37 -0700

    fixed routes denuncia

--- routes/denuncia.js
+++ routes/denuncia.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const router = express.Router();
 const denunciaController = require('../controllers/denunciaController.js');
 
-router.get('/', denunciaController.obtenerCita); // Cargar dato
-router.post('/', denunciaController.crearCita); // Traer dato
-router.get('/:id', denunciaController.obtenerCita); // Cargar dato
+router.get('/', denunciaController.obtenerDenuncia); // Cargar dato
+router.post('/', denunciaController.crearDenuncia); // Traer dato
+router.get('/:id', denunciaController.obtenerDenuncia); // Cargar dato
 module.exports = router;
