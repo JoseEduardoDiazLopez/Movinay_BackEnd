61fefee45a875120a800e7d0632d52fdad21be99
SergioAlejandroGuzmanAlvarez <sealguzmanal@ittepci.edu.mx>
2023-10-21 13:45:50 -0700

    denuncias update

--- models/Denuncias.js
+++ models/Denuncias.js
@@ -3,6 +3,9 @@ const DenunciaSchema = new mongoose.Schema({
     idDenuncia: {type: Number},
     Fecha: {type: Date, default: Date.now()},
     Descripcion : {type: String},
-    TipoDenuncia: {type: String}
+    Foto: {type: String},
+    Calles: {type: String},
+    Colonia: {type: String},
+    Municipio : {type: String}
   });
   module.exports = mongoose.model('Denuncia', DenunciaSchema);
