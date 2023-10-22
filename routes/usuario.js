c6af53b18af2f3a4a62d6f1660cf9bebe466705f
SergioAlejandroGuzmanAlvarez <sealguzmanal@ittepci.edu.mx>
2023-10-21 22:44:26 -0700

    cambios json

--- routes/usuario.js
+++ routes/usuario.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
 
-router.post('/register', usuarioController.register);
+router.post('/registro', usuarioController.register);
 router.post('/login', usuarioController.login);
 
 module.exports = router;
