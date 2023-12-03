f3d874b7ccfd5128020028752f580cbefca42ad4
SergioAlejandroGuzmanAlvarez <sealguzmanal@ittepic.edu.mx>
2023-12-03 11:15:41 -0700

    ultimos cambios payment

--- routes/pagos.js
+++ routes/pagos.js
@@ -5,6 +5,7 @@ const pagosController = require('../controllers/pagosController.js');
 router.post('/', pagosController.crearPago); // Cargar dato
 router.get('/idUsuario/:id', pagosController.obtenerPagoPorUsuarioId); // Traer dato
 router.put('/finish-payment/:idUsuario', pagosController.completarInformacionPago);
+//router.put('/finish-payment/:idUsuario', pagosController.completarInformacionPago);
 //router.put('/:id', pagosController.actualizarPago); // Modificar dato con ID
 //router.get('/:id', pagosController.obtenerPagoID); // Traer dato con ID
 //router.delete('/:id', pagosController.eliminarPago); // Eliminar dato con ID
