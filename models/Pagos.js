006b971af326b18a02572ea248ae06c151acdd86
SergioAlejandroGuzmanAlvarez <sealguzmanal@ittepic.edu.mx>
2023-12-03 21:14:16 -0700

    cambios api

--- models/Pagos.js
+++ models/Pagos.js
@@ -13,30 +13,4 @@ const PagosSchema = new mongoose.Schema({
     CVV : String,
     EstadoPago: String
 });
-/*PagosSchema.pre('save', async function (next) {
-    const doc = this;
-
-    if (!doc.idPago) {
-        try {
-            const latestPago = await mongoose.model('Pagos', PagosSchema).findOne({}, {}, { sort: { 'idPago': -1 } });
-            if (latestPago) {
-                doc.idPago = latestPago.idPago + 1;
-            } else {
-                doc.idPago = 1; // Si no hay pagos en la base de datos, empieza desde 1
-            }
-        } catch (error) {
-            return next(error);
-        }
-    }
-
-    if (!doc.numReferencia) {
-        const referenciaBase = 'TPCNAY00-'; // Parte fija de la referencia
-        const randomNumber = Math.floor(1000 + Math.random() * 9000); // Genera un número aleatorio de 4 dígitos
-
-        doc.numReferencia = referenciaBase + randomNumber;
-    }
-
-    next();
-});*/
-
 module.exports = mongoose.model('Pagos', PagosSchema);
