33b4bf3f9428570d19247f1c74468dec9e1c6a72
SergioAlejandroGuzmanAlvarez <sealguzmanal@ittepic.edu.mx>
2023-12-03 21:12:39 -0700

    cambios api

--- controllers/pagosController.js
+++ controllers/pagosController.js
@@ -20,22 +20,6 @@ exports.obtenerPagos = async (req,res) =>{
       res.status(500).send('Hubo un error al obtener pagos.')
   }
 }
-/*exports.obtenerPagoPorUsuarioId = async (req, res) => {
-    const idUsuario = req.params.id; // Suponiendo que el ID del usuario está en los parámetros de la solicitud
-
-    try {
-        console.log('ID de usuario:', idUsuario); // Verificar que el ID del usuario está llegando correctamente
-
-        const pagos = await Pagos.find({ idUsuario: idUsuario });
-
-        console.log('Pagos encontrados:', pagos); // Ver si se encuentran pagos relacionados con ese usuario
-
-        res.send(pagos);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('[ERROR] No se pudieron obtener los pagos del usuario.');
-    }
-};*/
 exports.obtenerPagoPorUsuarioId = async (req, res) => {
     const idUsuario = req.params.id; // Suponiendo que el ID del usuario está en los parámetros de la solicitud
 
--- models/PedidosTramites.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const PedidosTramitesSchema = new Schema({
-    idTramite: {type:Number},
-    numTramite: {type:Number},
-    fechaTramite: {type: Date, default: Date.now},
-    idUsuario: {type:Number}
-  });
-  module.exports = mongoose.model('Pedidos', PedidosTramitesSchema);
